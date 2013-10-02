c108ef005b1a604eaf9ac49b7bf19a4c74fe5151
Crystal McDonald <stalmcdonald@fullsail.edu>
2013-10-01 19:50:46 -0700

    Project Creation
    Set Sail San Francisco Web Activity shares a guide to sailing in San
    Francisco.

---
+++ src/com/cm/setsail/WebActivity.java
@@ -0,0 +1,30 @@
+/*
+ * Crystal McDonald
+ * MDF 3
+ * 1310
+ * Web Activity
+ * Set Sail app provides a web page with information for sailing in San Francisco
+ */
+
+package com.cm.setsail;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class WebActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_web);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_web, menu);
+		return true;
+	}
+
+}
